9fd001b7fa24426644dddd777d9d31b3e9d4f3be
David Ghena <davidadokuru@gmail.com>
2021-04-26 10:34:59 +0100

    Javacript code to run every 2 seconds

---
+++ main.js
@@ -0,0 +1,24 @@
+//This will run every 2 seconds you can test the increase to a mintue
+
+setInterval(function () {
+  fetchData();
+}, 20000);
+function fetchData() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(function (response) {
+      // The API call was successful!
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(function (data) {
+      // This is the JSON from our response
+      console.log(data);
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
+}
